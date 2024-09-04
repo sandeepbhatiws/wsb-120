fc1e5320a7cc7d55762942abff5c5d5001c01728
sandeep <sandeep@gmail.com>
2024-09-04 18:45:13 +0530

    changes

--- react/second-react-project/src/Components/Home.jsx
+++ react/second-react-project/src/Components/Home.jsx
@@ -6,6 +6,9 @@ export default function Home() {
 
     var heading = 'Home Page';
 
+    var homeContent = 'His mother had always taught him';
+    var description = "His mother had always taught him not to ever think of himself as better than others. He'd tried to live by this motto. He never looked down on those who were less fortunate or who had less money than him. But the stupidity of the group of people he was talking to made him change his mind.";
+
     var finalPosts = posts.map((v,i) => {
         return(
             <div className="column" key={i}>
@@ -38,9 +41,25 @@ export default function Home() {
 
                 {/* Second Method */}
 
-                { finalPosts }
+                {/* { finalPosts } */}
 
-                {/* <HomeContent/>
+                {/* Third Method */}
+                {
+                    posts.map((v,i) => {
+                        return(
+                            <HomeContent data={v} key={i}/>
+                        )
+                    })
+                }
+
+
+                {/* <HomeContent heading={ homeContent } description = {description}>
+
+                    His mother had always taught him
+
+                </HomeContent> */}
+
+                {/* 
                 <HomeContent/>
                 <HomeContent/>
                 <HomeContent/> */}
